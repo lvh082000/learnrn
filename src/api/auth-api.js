5096c6eb1b2851e1a997b3477e47d89158a4e724
HieuLV <lvh082000@gmail.com>
2022-08-26 23:23:43 +0700

    add forgot-password, reset-password

--- src/api/auth-api.js
+++ src/api/auth-api.js
@@ -3,6 +3,15 @@ import axiosClient from './axios-client';
 export const login = async payload => {
   return await axiosClient.post('/auth/login', payload);
 };
+
 export const register = async payload => {
   return await axiosClient.post('/auth/register', payload);
+};
+
+export const forgotPassword = async (payload) => {
+	return await axiosClient.post('/auth/forgot-password', payload);
+};
+
+export const resetPassword = async (payload) => {
+	return await axiosClient.post('/auth/reset-password', payload);
 };
